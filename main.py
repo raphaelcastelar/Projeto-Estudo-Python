3eb07817a84f393efb0f14147ffee1bd08235a7c
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-03-07 09:23:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyautogui
+import time
 
 pyautogui.PAUSE = 0.5
 
@@ -8,4 +9,6 @@ pyautogui.press("enter")
 pyautogui.write("https://stackoverflow.com/questions/31635140/import-error-for-pyautogui")
 pyautogui.press("enter")
 
+time.sleep(3)
 
+pyautogui.click()
