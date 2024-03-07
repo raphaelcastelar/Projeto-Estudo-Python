93a8749ea1601194fac962e5ae6435a77b3d31ba
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-03-07 08:52:10 -0300

    att automação

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print ("Olá Mundo")
+import pyautogui
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press("win")
+pyautogui.write("edge")
+pyautogui.press("enter")
+pyautogui.write("https://stackoverflow.com/questions/31635140/import-error-for-pyautogui")
+pyautogui.press("enter")
