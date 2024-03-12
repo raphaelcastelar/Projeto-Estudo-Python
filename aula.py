21605306c47f46aa6a53ab8980226f31b5d349c4
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-03-12 10:44:05 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -1,5 +1,4 @@
 
-
 meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul','ago', 'set', 'out', 'nov', 'dez']
 vendas_1sem = [25000, 29000, 22200, 17750, 15870, 19900]
 vendas_2sem = [19850, 20120, 17540, 15555, 49051, 9650]
@@ -11,7 +10,6 @@ menor_venda = min(vendas_ano)
 
 indice_maior_valor = vendas_ano.index(maior_venda)
 indice_menor_valor = vendas_ano.index(menor_venda)
-
 
 
 print(f"O melhor mês do ano foi {meses[indice_maior_valor]} com {maior_venda} vendas")
