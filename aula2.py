6445e355e9d203c152f9c08411ac792341a0d5f0
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-03-13 10:27:15 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,17 @@
+
+produtos = ['computador', 'livro', 'tablet', 'celular', 'tv', 'ar condicionado', 'alexa', 'maquina de café', 'kindle']
+
+produtos_ecommerce = [[10000, 2500],
+                     [50000, 40],
+                     [7000, 1200],
+                     [20000, 1500],
+                     [5800, 1300],
+                     [7200, 2500],
+                     [200,800],
+                     [3300, 700],
+                     [1900,400]]
+
+indice_livro = produtos_ecommerce[1][1]
+produtos_ecommerce[1][1] = indice_livro * 0.1 + indice_livro
+print(produtos_ecommerce)
+print(f"O aumento do custo mensal vai ser em média {(produtos_ecommerce[1][1] * 0.1) * produtos_ecommerce[1][0]}")
