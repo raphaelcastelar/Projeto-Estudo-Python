aaceb199f0276560033f606d8d38412c4719893a
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-03-14 09:14:12 -0300

    Update Exfor.py

--- Exfor.py
+++ Exfor.py
@@ -2,7 +2,11 @@ qtde_pessoas = int(input("Quantas pessoas terão no quarto? "))
 quarto = []
 
 for i in range(qtde_pessoas):
-    quarto.append(input("Qual o Seu nome? "))
-    quarto.append(input("Qual o CPF? "))
+  
+    nome = input("Qual o nome: ")
+    cpf = input("Qual o CPF: ")
+    hospede = [nome, "CPF: " + cpf]
+    quarto.append(hospede)
+
 
 print(quarto)
