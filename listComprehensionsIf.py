f977e97c8a4383147fc3d790988e52493b1587e2
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-06-30 11:26:05 -0300

    exlistcomprehensionif

---
+++ listComprehensionsIf.py
@@ -0,0 +1,15 @@
+meta = 1000
+vendas_produtos = [1500, 150, 2100, 1950]
+produtos = ['vinho', 'cafeteira', 'microondas', 'iphone']
+
+produtos_bateram_meta = []
+
+for i, produto in enumerate(produtos):
+    if vendas_produtos[i] >= meta:
+        produtos_bateram_meta.append(produto)
+
+print(produtos_bateram_meta)
+
+list_aux = zip(vendas_produtos, produtos)
+list_compre =  [produtos1 for vendas, produtos1 in list_aux if vendas >= meta]
+print(list_compre)
