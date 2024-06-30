6fe6eb86215f0c0da92574955a0d680d38876526
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-06-30 11:05:31 -0300

    ex list comprehension

--- exListComp1.py
+++ exListComp1.py
@@ -3,3 +3,7 @@ vendas_produtos = [('iphone', 558147, 951642), ('galaxy', 712350, 244295)]
 valores2019 = [vendas2019 for produto, vendas2019, vendas2020 in vendas_produtos]
 
 print(valores2019)
+print(max(valores2019))
+
+lista_vendasproduto2019 = [(produto, vendas2019) for produto, vendas2019, vendas2020 in vendas_produtos]
+print(max(lista_vendasproduto2019))
