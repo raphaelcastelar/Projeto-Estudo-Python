3eda0cb709d75315b2eae395090634ab8d35ea1d
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-06-30 11:06:15 -0300

    Update exListComp1.py

--- exListComp1.py
+++ exListComp1.py
@@ -5,5 +5,5 @@ valores2019 = [vendas2019 for produto, vendas2019, vendas2020 in vendas_produtos
 print(valores2019)
 print(max(valores2019))
 
-lista_vendasproduto2019 = [(produto, vendas2019) for produto, vendas2019, vendas2020 in vendas_produtos]
+lista_vendasproduto2019 = [(vendas2019, produto) for produto, vendas2019, vendas2020 in vendas_produtos]
 print(max(lista_vendasproduto2019))
