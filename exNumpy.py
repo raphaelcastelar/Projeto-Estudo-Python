9344b7f3236aea83384e7f4ce1f8dfc3abd7f39f
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-07-04 09:35:29 -0300

    exNumpy2

--- exNumpy.py
+++ exNumpy.py
@@ -5,3 +5,9 @@ dados = [ 127, 90, 201, 150, 210, 220, 115]
 vendas = np.array(dados)
 media = np.mean(vendas)
 print(media)
+
+precos = np.array([31.40, 31.25, 30.95, 31.20, 31.60, 31.50])
+preco_maximo = np.max(precos)
+preco_minimo = np.min(precos)
+variacao_preco = preco_maximo - preco_minimo
+print("O preço máximo foi de {}, o mínimo foi de {}, e a variação foi de {}".format(preco_maximo,preco_minimo,variacao_preco))
